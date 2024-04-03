ceb8442d22f5a2b08b0b96c7f54e1a015c58ead2
Scott McIntire <scottrsm@gmail.com>
2024-04-03 16:37:45 -0400

    Added github doc and test actions

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,18 @@
-using Documenter, Cluster
+using Cluster
+import Pkg
 
-makedocs(sitename="Cluster Docs")
+Pkg.add("Documenter")
+using Documenter
 
+makedocs(
+	sitename = "Cluster",
+	format = Documenter.HTML(),
+	modules = [Cluster]
+	)
+
+	# Documenter can also automatically deploy documentation to gh-pages.
+	# See "Hosting Documentation" and deploydocs() in the Documenter manual
+	# for more information.
+	deploydocs(
+		repo = "github.com/scottrsm/Cluster.jl.git"
+	)
